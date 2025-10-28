fe7a2cc6983a361b02d2b4abd224f1f802fd66cd
Umer-Jahangir <umerjahangir.cs@gmail.com>
2025-10-28 19:46:16 +0500

    Fix Vercel 404 issue with rewrites and vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // Vercel expects your build output here
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
+});
